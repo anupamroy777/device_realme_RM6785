f8b498bc9f3a653f1154395d3ae5157a5ae24403
jjpprrrr <jjpprrrr@pixelexperience.org>
2021-12-19 20:58:49 +0530

    RM6785: Enable Iorap prefetch

--- properties.mk
+++ properties.mk
@@ -27,3 +27,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
+
+# Iorap
+persist.device_config.runtime_native_boot.iorap_readahead_enable=true
+iorapd.readahead.enable=true
